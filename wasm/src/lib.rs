3b5f80c9e1100550bfc3957d84562ee396ab3d33
hudson-newey <hudsonnewey@gmail.com>
2025-08-01 00:40:26 +1000

    First render of a square

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,5 +1,6 @@
 use wasm_bindgen::prelude::*;
 
+#[derive(Clone)]
 struct RgbaColor {
     r: u8,
     g: u8,
@@ -12,7 +13,12 @@ pub fn next_frame(current_frame: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
     let background: Vec<u8> = for_each_pixel(
         current_frame,
         |_| true,
-        |_| RgbaColor{ r: 0, g: 0, b: 0, a: 255 },
+        |_| RgbaColor {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: 255,
+        },
     );
 
     let cube_size: u32 = 600;
@@ -25,15 +31,17 @@ pub fn next_frame(current_frame: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
     let square: Vec<u8> = draw_surface(
         background,
         RgbaColor {
-            r: 0,
-            g: 0,
-            b: 200,
-            a: 200,
+            r: 20,
+            g: 10,
+            b: 80,
+            a: 255,
         },
+        width,
+        height,
         cube_left,
-        cube_size,
+        cube_left + cube_size,
         cube_top,
-        cube_size,
+        cube_top + cube_size,
         0,
         0,
     );
@@ -42,22 +50,25 @@ pub fn next_frame(current_frame: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
 }
 
 fn pixel_to_rgba(buffer: &[u8]) -> RgbaColor {
-    RgbaColor { r: buffer[0], g: buffer[1], b: buffer[2], a: buffer[3] }
+    RgbaColor {
+        r: buffer[0],
+        g: buffer[1],
+        b: buffer[2],
+        a: buffer[3],
+    }
 }
 
 // A really slow helper to conditionally apply a function to every pixel.
 fn for_each_pixel<When, Action>(data: Vec<u8>, when: When, action: Action) -> Vec<u8>
 where
-    When: Fn(usize) -> bool,
+    When: Fn(u32) -> bool,
     Action: Fn(&RgbaColor) -> RgbaColor,
 {
     let mut result: Vec<u8> = Vec::with_capacity(data.len());
 
     for (index, pixel) in data.chunks(4).enumerate() {
-        if when(index) {
-            let color: RgbaColor = action(
-                &pixel_to_rgba(pixel)
-            );
+        if when(index as u32) {
+            let color: RgbaColor = action(&pixel_to_rgba(pixel));
 
             result.push(color.r);
             result.push(color.g);
@@ -74,6 +85,8 @@ where
 fn draw_surface(
     data: Vec<u8>,
     color: RgbaColor,
+    width: u32,
+    height: u32,
     x0: u32,
     x1: u32,
     y0: u32,
@@ -81,5 +94,13 @@ fn draw_surface(
     z0: u32,
     z1: u32,
 ) -> Vec<u8> {
-    return data;
+    let layer_data = for_each_pixel(
+        data,
+        |index| {
+            index > (y0 * width) && index < (y1 * width) && index % width > x0 && index % width < x1
+        },
+        |_| color.clone(),
+    );
+
+    layer_data
 }
