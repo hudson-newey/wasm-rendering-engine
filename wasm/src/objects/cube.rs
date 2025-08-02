633fdf6d936cd7ac2122d1639844c7ead83086ff
hudson-newey <hudsonnewey@gmail.com>
2025-08-02 22:34:20 +1000

    Fix inverted y camera calculations

--- wasm/src/objects/cube.rs
+++ wasm/src/objects/cube.rs
@@ -17,8 +17,8 @@ impl objects::drawable::Drawable for Cube {
     fn draw(&self, image: &ImageData, camera: &objects::camera::Camera) -> ImageData {
         let z_distance = self.pos.z - camera.pos.z;
 
-        let top_pos = self.pos.y + z_distance + camera.pos.y;
-        let bottom_pos = self.pos.y + (self.height as f64) - z_distance + camera.pos.y;
+        let top_pos = self.pos.y + z_distance - camera.pos.y;
+        let bottom_pos = self.pos.y + (self.height as f64) - z_distance - camera.pos.y;
 
         let left_pos = self.pos.x + z_distance - camera.pos.x;
         let right_pos = self.pos.x + (self.width as f64) - z_distance - camera.pos.x;
